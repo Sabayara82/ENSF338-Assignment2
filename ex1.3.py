24fd3e9d24f477b47fa3a4a8c1d5e07978dce0ad
Sabayara82 <113862210+Sabayara82@users.noreply.github.com>
2023-02-15 02:44:36 -0700

    Update ex1.3.py
    Memoization of the fibonacci sequence code

--- ex1.3.py
+++ ex1.3.py
@@ -1,22 +1,11 @@
+import time
 
-import matplotlib.pyplot as plt
 
-import time 
-def func(n):
+def func1(n, memo={}):
     if n == 0 or n == 1:
         return n
+    if n in memo:
+        return memo[n]
     else:
-        return func(n-1) + func(n-2)
-    
-times = []
-for n in range(36):
-    start = time.time()
-    result = func(n)
-    end = time.time()
-    times.append(end - start)
-
-plt.plot(range(36), times)
-plt.xlabel("Input size (n)")
-plt.ylabel("Time (seconds)")
-plt.title("Time complexity of func()")
-plt.show()
+        memo[n] = func1(n-1, memo) + func1(n-2, memo)
+        return memo[n]
